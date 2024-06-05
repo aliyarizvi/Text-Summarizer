782317b41f53468366aa25124b23f5d8872f3a26
aliyarizvi <aliyarizvi28@gmail.com>
2024-06-05 05:38:04 +0530

    Model trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,67 @@
+from transformers import TrainingArguments, Trainer
+from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from datasets import load_from_disk
+from dataclasses import dataclass
+import torch
+import os
+from src.logger import logging
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: str = os.path.join('data')
+    data_path: str = os.path.join('data_transformation/samsum_dataset')
+    model_ckpt: str = "google/pegasus-cnn_dailymail"
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+
+    def train(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        tokenizer = AutoTokenizer.from_pretrained(self.config.model_ckpt)
+        model_pegasus = AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
+        seq2seq_data_collator = DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)
+        
+        dataset_samsum_pt = load_from_disk(self.config.data_path)
+
+        trainer_args = TrainingArguments(
+            output_dir=self.config.root_dir,
+            num_train_epochs=1,
+            warmup_steps=500,
+            per_device_train_batch_size=1,
+            per_device_eval_batch_size=1,
+            weight_decay=0.01,
+            logging_steps=10,
+            evaluation_strategy='steps',
+            eval_steps=500,
+            save_steps=1e6,
+            gradient_accumulation_steps=16
+        ) 
+
+        trainer = Trainer(
+            model=model_pegasus,
+            args=trainer_args,
+            tokenizer=tokenizer,
+            data_collator=seq2seq_data_collator,
+            train_dataset=dataset_samsum_pt["train"],
+            eval_dataset=dataset_samsum_pt["validation"]
+        )
+        
+        trainer.train()  
+
+        # Save model
+        model_save_path = os.path.join(self.config.root_dir, "pegasus-samsum-model")
+        tokenizer_save_path = os.path.join(self.config.root_dir, "tokenizer")
+        model_pegasus.save_pretrained(model_save_path)
+        tokenizer.save_pretrained(tokenizer_save_path)
+        
+        logging.info(f"Model saved to {model_save_path}")
+        logging.info(f"Tokenizer saved to {tokenizer_save_path}")
+
+if __name__ == "__main__":
+    config = ModelTrainerConfig()
+    model_trainer = ModelTrainer(config)
+    model_trainer.train()
+
+    
